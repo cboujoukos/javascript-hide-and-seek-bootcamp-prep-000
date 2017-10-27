ba69632e7fa4f483a76aaf07000227051d86b567
Christopher Michael Boujoukos <cboujoukos@gmail.com>
2017-10-27 18:33:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ function increaseRankBy(n) {
   //access each element li that is part of an element ul class='ranked-list'
   //turn each value into a number with parseInt()
   //add n to each value
-  //to access each desired element I need to use
   const rankings = document.querySelectorAll('.ranked-list li')
   for (let i=0; i<rankings.length; i++) {
     rankings[i].innerHTML = parseInt(rankings[i].innerHTML) + n;
   }
 }
 function deepestChild() {
-  
+  let deepest = document.querySelectorAll('#grand-node div');
+  console.log(deepest[deepest.length - 1])
 }
