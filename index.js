788583124d15d871b53bc9ea486f4d0759bfd55f
Christopher Michael Boujoukos <cboujoukos@gmail.com>
2017-10-27 19:05:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+function increaseRankBy(n) {
+  //access each element li that is part of an element ul class='ranked-list'
+  //turn each value into a number with parseInt()
+  //add n to each value
+  const rankings = document.querySelectorAll('.ranked-list li')
+  for (let i=0; i<rankings.length; i++) {
+    rankings[i].innerHTML = parseInt(rankings[i].innerHTML) + n;
+  }
+}
+function deepestChild() {
+  let deepest = document.querySelectorAll('#grand-node div');
+  return deepest[deepest.length - 1]
+}
